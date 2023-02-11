95c8ba55af6afbc7c3a4384e3a5feeaed15dfcfe
andresq99 <andres.quelal@yachaytech.edu.ec>
2023-02-11 17:34:32 -0500

    path_operations_structure_users

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from datetime import date
 from datetime import datetime
 from typing import Optional
+from typing import List
 
 # pydantic
 from pydantic import BaseModel
@@ -11,6 +12,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
@@ -43,7 +45,6 @@ class User(UserBase):
     )
     birth_date: Optional[date] = Field(default=None)
     
-
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)  
     content : str = Field(
@@ -64,3 +65,73 @@ class Tweet(BaseModel):
 def home():
     return {"Twitter API": "Working"}
 
+## Users
+
+### SignUp
+@app.post(
+    path="/signup",
+    response_model = User, # Responde con la informacion del usuario
+    status_code=status.HTTP_201_CREATED, # Si el usuario crea correctamente su cuenta
+    summary="Register a New User",
+    tags=["Users"] 
+)
+def signup(): # Registrar un usuario
+    pass
+
+### Login
+@app.post(
+    path="/login",
+    response_model = User, # Responde con la informacion del usuario
+    status_code=status.HTTP_200_OK, # Si el usuario crea correctamente su cuenta
+    summary="Login a User",
+    tags=["Users"] 
+)
+def login(): # Registrar un usuario
+    pass
+
+### Show all users
+@app.get(
+    path="/users",
+    response_model = List["User"], # Responde un JSON con un formato de lista
+    status_code=status.HTTP_200_OK, # Si el usuario crea correctamente su cuenta
+    summary="Show all users",
+    tags=["Users"] 
+)
+def show_all_users(): # Registrar un usuario
+    pass
+
+### Show an user
+@app.get(
+    path="/user/{user_id}",
+    response_model = User, # Responde con la informacion del usuario
+    status_code=status.HTTP_200_OK, # Si el usuario crea correctamente su cuenta
+    summary="Show an user",
+    tags=["Users"] 
+)
+def show_an_user(): # Registrar un usuario
+    pass
+
+### Delete an user
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model = User, # Responde con la informacion del usuario
+    status_code=status.HTTP_200_OK, # Si el usuario crea correctamente su cuenta
+    summary="Delete an user",
+    tags=["Users"] 
+)
+def delete_an_user(): # Registrar un usuario
+    pass
+
+### Update an user
+@app.put(
+    path="/users/{user_id}/update",
+    response_model = User, # Responde con la informacion del usuario
+    status_code=status.HTTP_200_OK, # Si el usuario crea correctamente su cuenta
+    summary="Update an user",
+    tags=["Users"] 
+)
+def update_an_user(): # Registrar un usuario
+    pass
+
+
+## Tweets
