fe3c5d957d4c4eec1393caea83d4ee689a6bd226
andresq99 <andres.quelal@yachaytech.edu.ec>
2023-02-11 17:04:31 -0500

    models_tweets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Python
 from uuid import UUID
 from datetime import date
+from datetime import datetime
 from typing import Optional
 
 # pydantic
@@ -17,31 +18,43 @@ app = FastAPI()
 
 class UserBase(BaseModel): # Tiene la informacion basica de un usuario
     user_id: UUID = Field(...) 
-    email: EmailStr = Field(...)
+    email: EmailStr = Field(..., example="andres@gmail.com")
     pass
 
 class UserLogin(UserBase):
     password: str = Field( # Nunca es bueno responder al cliente de la API con la contraseña
         ..., 
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 class User(UserBase): 
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Andres"
     )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Quelal"
     )
     birth_date: Optional[date] = Field(default=None)
     
 
 class Tweet(BaseModel):
-    pass    
+    tweet_id: UUID = Field(...)  
+    content : str = Field(
+        ...,
+        min_length=1,
+        max_length=256,
+        example="Hello World"
+    )
+    created_at: datetime = Field(default=datetime.now())  
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...) # Relacion de uno a muchos
 
 
 
